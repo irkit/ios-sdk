dce1bba66f5c918024b8452a621e66939cf180f8
Masakazu OHTSUKA <o.masakazu@gmail.com>
2013-05-17 18:17:29 +0900

    initial ios

---
+++ ios/IRKit/IRKit/IRKit.m
@@ -0,0 +1,13 @@
+//
+//  IRKit.m
+//  IRKit
+//
+//  Created by Masakazu Ohtsuka on 2013/05/17.
+//  Copyright (c) 2013年 KAYAC Inc. All rights reserved.
+//
+
+#import "IRKit.h"
+
+@implementation IRKit
+
+@end
