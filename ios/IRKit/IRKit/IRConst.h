451e11703451b0a6e4d42cedabfa3aa9b35322b5
Masakazu OHTSUKA <o.masakazu@gmail.com>
2013-05-29 16:13:38 +0900

    こまごま

--- ios/IRKit/IRKit/IRConst.h
+++ ios/IRKit/IRKit/IRConst.h
@@ -24,17 +24,20 @@
 // see https://www.bluetooth.org/en-us/specification/assigned-numbers-overview/service-discovery
 #define IRKIT_SERVICE_BASE_UUID (@"00000000-0000-1000-8000-00805F9B34FB")
 
-// org.bluetooth.service.link_loss
-#define IRKIT_SERVICE_LINK_LOSS        0x1803
-
 // org.bluetooth.service.immediate_alert
-#define IRKIT_SERVICE_IMMEDIATE_ALERT  0x1802
+#define IRKIT_SERVICE_IMMEDIATE_ALERT    0x1802
+
+// org.bluetooth.service.link_loss
+#define IRKIT_SERVICE_LINK_LOSS          0x1803
 
 // org.bluetooth.service.tx_power
-#define IRKIT_SERVICE_TX_POWER         0x1804
+#define IRKIT_SERVICE_TX_POWER           0x1804
+
+// org.bluetooth.service.device_information
+#define IRKIT_SERVICE_DEVICE_INFORMATION 0x180A
 
 // org.bluetooth.service.battery_service
-#define IRKIT_SERVICE_BATTERY_SERVICE  0x180F
+#define IRKIT_SERVICE_BATTERY_SERVICE    0x180F
 
 #pragma mark -
 #pragma NSNotification names
