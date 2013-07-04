b3f80920d94b8daf1c4f5d78d9ced662a6841597
Masakazu OHTSUKA <o.masakazu@gmail.com>
2013-07-04 16:17:41 +0900

    added bluetooth-central background mode for testing

--- ios/SimpleRemote/SimpleRemote/SRAppDelegate.m
+++ ios/SimpleRemote/SimpleRemote/SRAppDelegate.m
@@ -13,6 +13,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    LOG_CURRENT_METHOD;
     // Override point for customization after application launch.
 
     [[IRKit sharedInstance] startScan];
@@ -28,22 +29,26 @@
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
+    LOG_CURRENT_METHOD;
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
+    LOG_CURRENT_METHOD;
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
+    LOG_CURRENT_METHOD;
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
 {
+    LOG_CURRENT_METHOD;
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
