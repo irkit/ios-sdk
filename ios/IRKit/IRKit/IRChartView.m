e0471d33d86a8aab15f957f6d6858b308bb582ad
Masakazu OHTSUKA <o.masakazu@gmail.com>
2013-08-01 19:19:01 +0900

    draw chartview with 1px line

--- ios/IRKit/IRKit/IRChartView.m
+++ ios/IRKit/IRKit/IRChartView.m
@@ -102,81 +102,21 @@
     return path;
 }
 
-- (CGMutablePathRef)chartBottomLinePath {
-    CGMutablePathRef path = CGPathCreateMutable();
-    double sum           = (double)[self sumOfData];
-    double intervalScale = self.frame.size.width / (INTERVAL_MARGIN_LEFT + INTERVAL_BEFORE_LEADER + sum + INTERVAL_AFTER_STOP + INTERVAL_MARGIN_RIGHT);
-    double topY          = MARGIN_TOP;
-    double bottomY       = self.frame.size.height - PADDING_BOTTOM;
-    double bottomLineY   = self.frame.size.height;
-
-    double rightX = INTERVAL_MARGIN_LEFT + INTERVAL_BEFORE_LEADER + sum + INTERVAL_AFTER_STOP;
-    LOG( @"rightX: %f", rightX);
-    // right-top
-    CGPathMoveToPoint(path, NULL,    rightX*intervalScale,
-                                     topY);
-    // ↓
-    CGPathAddLineToPoint(path, NULL, rightX*intervalScale,
-                                     bottomLineY);
-    // ←
-    rightX = INTERVAL_MARGIN_LEFT;
-    CGPathAddLineToPoint(path, NULL, rightX*intervalScale,
-                                     bottomLineY);
-    // ↑
-    CGPathAddLineToPoint(path, NULL, rightX*intervalScale,
-                                     topY);
-    return path;
-}
-
 -(void)drawInContext:(CGContextRef)context
 {
     LOG_CURRENT_METHOD;
-    
-    // set clip using chart path
-    CGMutablePathRef topLinePath    = [self chartTopLinePath];
-    CGMutablePathRef bottomLinePath = [self chartBottomLinePath];
-    CGContextAddPath(context,topLinePath);
-    CGContextAddPath(context,bottomLinePath);
-    CGPathRef outlinePath = CGPathCreateCopyByStrokingPath(topLinePath, NULL, 1, kCGLineCapButt, kCGLineJoinMiter, 0);
-    CGPathRelease(topLinePath);
-    CGPathRelease(bottomLinePath);
 
-    CGContextSaveGState(context); // clip start
-    CGContextClip(context);
-    
-    // gradient
-    CGColorSpaceRef colorSpaceRef = CGColorSpaceCreateDeviceRGB();
-    CGFloat components[] = {
-        0.f, 0.f, 0.f, 0.f,     // R, G, B, Alpha
-        0.f, 0.f, 0.f, 0.f
-    };
-    CGFloat locations[] = { 0.0f, 1.0f };
-    size_t count = sizeof(components)/ (sizeof(CGFloat)* 4);
-    
-    CGPoint startPoint = {.x = 0, .y = 0. };
-    CGPoint endPoint   = {.x = 0, .y = self.frame.size.height };
-    
-    CGGradientRef gradientRef =
-    CGGradientCreateWithColorComponents(colorSpaceRef, components, locations, count);
-    
-    CGContextDrawLinearGradient(context,
-                                gradientRef,
-                                startPoint,
-                                endPoint,
-                                0);
-    
-    CGGradientRelease(gradientRef);
-    CGColorSpaceRelease(colorSpaceRef);
-    
-    CGContextRestoreGState(context); // clip end
-
-    // add outline
-    CGContextAddPath(context, outlinePath);
     CGContextSetStrokeColorWithColor(context, [UIColor cyanColor].CGColor);
     CGContextSetFillColorWithColor(context, [UIColor clearColor].CGColor);
-    CGContextSetLineWidth(context, 1);
-    CGContextDrawPath(context, kCGPathFillStroke);
-    CGPathRelease(outlinePath);
+    CGContextSetLineWidth(context, 0.5);
+    CGContextSetShouldAntialias(context, NO);
+    CGContextSetAllowsAntialiasing(context, NO);
+
+    CGMutablePathRef topLinePath    = [self chartTopLinePath];
+    CGContextAddPath(context,topLinePath);
+    CGContextAddPath(context,topLinePath);
+    CGContextDrawPath(context, kCGPathStroke);
+    CGPathRelease(topLinePath);
 }
 
 @end
