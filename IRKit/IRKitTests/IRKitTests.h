6a8e147992921466672648ac357dcfac181b5d7f
Masakazu OHTSUKA <o.masakazu@gmail.com>
2014-02-17 18:37:23 +0900

    recreate tests using XCUnit

--- IRKit/IRKitTests/IRKitTests.h
+++
@@ -1,5 +0,0 @@
-#import <SenTestingKit/SenTestingKit.h>
-
-@interface IRKitTests : SenTestCase
-
-@end
---
+++ IRKit/IRKitTests/IRSignalTests.m
@@ -0,0 +1,34 @@
+//
+//  IRSignalTests.m
+//  IRKit
+//
+//  Created by Masakazu Ohtsuka on 2014/02/17.
+//
+//
+
+#import <XCTest/XCTest.h>
+
+@interface IRSignalTests : XCTestCase
+
+@end
+
+@implementation IRSignalTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here; it will be run once, before the first test case.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here; it will be run once, after the last test case.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
