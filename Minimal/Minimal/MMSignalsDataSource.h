cffc7cf320a3a7c0ca27fa786226cd3fe5cc7cd4
Masakazu OHTSUKA <o.masakazu@gmail.com>
2014-02-17 17:29:08 +0900

    k is not involved anymore

--- Minimal/Minimal/MMSignalsDataSource.h
+++ Minimal/Minimal/MMSignalsDataSource.h
@@ -1,11 +1,3 @@
-//
-//  MMSignalsDataSource.h
-//  Minimal
-//
-//  Created by Masakazu Ohtsuka on 2014/01/03.
-//  Copyright (c) 2014年 KAYAC Inc. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 #import <IRKit/IRKit.h>
 
--- Minimal/Minimal/MMSignalsDataSource.m
+++ Minimal/Minimal/MMSignalsDataSource.m
@@ -1,11 +1,3 @@
-//
-//  MMSignalsDataSource.m
-//  Minimal
-//
-//  Created by Masakazu Ohtsuka on 2014/01/03.
-//  Copyright (c) 2014年 KAYAC Inc. All rights reserved.
-//
-
 #import "MMSignalsDataSource.h"
 
 @interface MMSignalsDataSource ()
